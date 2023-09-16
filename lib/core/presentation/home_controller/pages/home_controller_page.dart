d0725a789845f395f6e726862052d9bec6810540
Alessandro Cavaliere <72750724+alecava41@users.noreply.github.com>
2023-09-16 10:54:36 +0200

    final fix on status_bar for iOS

--- lib/core/presentation/home_controller/pages/home_controller_page.dart
+++ lib/core/presentation/home_controller/pages/home_controller_page.dart
@@ -34,69 +34,67 @@ class HomeControllerScreen extends StatelessWidget {
       builder: (badgeCtx, badgeState) => BlocBuilder<HomeControllerBloc, HomeControllerState>(
         builder: (ctx, state) => BlocBuilder<SearchBloc, SearchState>(
           builder: (searchCtx, searchState) {
-            return SafeArea(
-              child: Scaffold(
-                appBar: PreferredSize(
-                  preferredSize: const Size.fromHeight(0),
-                  child: AppBar(
-                    backgroundColor: state.tabIndex == 4 ||
-                            (state.tabIndex == 1 && searchState.pageState == SearchPageState.resultPage)
-                        ? Theme.of(context).extension<CustomColors>()!.background2
-                        : Theme.of(context).colorScheme.background,
-                    elevation: 0,
-                    surfaceTintColor: Theme.of(context).colorScheme.outline,
-                    shadowColor: Theme.of(context).colorScheme.outline,
-                    iconTheme: IconThemeData(color: Theme.of(context).colorScheme.onBackground),
-                  ),
+            return Scaffold(
+              appBar: PreferredSize(
+                preferredSize: const Size.fromHeight(0),
+                child: AppBar(
+                  backgroundColor:
+                      state.tabIndex == 4 || (state.tabIndex == 1 && searchState.pageState == SearchPageState.resultPage)
+                          ? Theme.of(context).extension<CustomColors>()!.background2
+                          : Theme.of(context).colorScheme.background,
+                  elevation: 0,
+                  surfaceTintColor: Theme.of(context).colorScheme.outline,
+                  shadowColor: Theme.of(context).colorScheme.outline,
+                  iconTheme: IconThemeData(color: Theme.of(context).colorScheme.onBackground),
                 ),
-                backgroundColor: Theme.of(context).colorScheme.background,
-                body: pages[state.tabIndex],
-                bottomNavigationBar: BottomNavigationBar(
-                  showSelectedLabels: true,
-                  type: BottomNavigationBarType.fixed,
-                  showUnselectedLabels: true,
-                  currentIndex: state.tabIndex,
-                  onTap: (index) {
-                    if (!hasPageBeenInitialized[index]) {
-                      switch (index) {
-                        case 3:
-                          ctx.read<InboxBloc>().add(const InboxEvent.inboxContentCreated());
-                        case 4:
-                          ctx.read<UserBloc>().add(const UserEvent.contentCreated());
-                      }
-
-                      hasPageBeenInitialized[index] = true;
+              ),
+              backgroundColor: Theme.of(context).colorScheme.background,
+              body: SafeArea(child: pages[state.tabIndex]),
+              bottomNavigationBar: BottomNavigationBar(
+                showSelectedLabels: true,
+                type: BottomNavigationBarType.fixed,
+                showUnselectedLabels: true,
+                currentIndex: state.tabIndex,
+                onTap: (index) {
+                  if (!hasPageBeenInitialized[index]) {
+                    switch (index) {
+                      case 3:
+                        ctx.read<InboxBloc>().add(const InboxEvent.inboxContentCreated());
+                      case 4:
+                        ctx.read<UserBloc>().add(const UserEvent.contentCreated());
                     }
 
-                    ctx.read<HomeControllerBloc>().add(HomeControllerEvent.tabChanged(index));
-                  },
-                  backgroundColor: Theme.of(context).extension<CustomColors>()!.background2,
-                  selectedItemColor: Theme.of(context).colorScheme.primary,
-                  unselectedItemColor: Theme.of(context).colorScheme.outline,
-                  items: [
-                    BottomNavigationBarItem(
-                        icon: badges.Badge(
-                          position: badges.BadgePosition.topEnd(top: 0, end: -1),
-                          showBadge: badgeState.unreadReceivedClaims > 0 || badgeState.unreadNews > 0,
-                          child: const Icon(Icons.home),
-                        ),
-                        label: AppLocalizations.of(context)!.homeBottomBarTab),
-                    BottomNavigationBarItem(
-                        icon: const Icon(Icons.search), label: AppLocalizations.of(context)!.searchBottomBarTab),
-                    BottomNavigationBarItem(
-                        icon: const Icon(Icons.add_circle_outline_outlined),
-                        label: AppLocalizations.of(context)!.insertBottomBarTab),
-                    BottomNavigationBarItem(
-                        icon: badges.Badge(
-                          showBadge: badgeState.hasUnreadChats,
-                          position: badges.BadgePosition.topEnd(top: 0, end: -1),
-                          child: const Icon(Icons.mail),
-                        ),
-                        label: AppLocalizations.of(context)!.inboxBottomBarTab),
-                    BottomNavigationBarItem(
-                        icon: const Icon(Icons.person), label: AppLocalizations.of(context)!.profileBottomBarTab),
-                  ],
-                ),
+                    hasPageBeenInitialized[index] = true;
+                  }
+
+                  ctx.read<HomeControllerBloc>().add(HomeControllerEvent.tabChanged(index));
+                },
+                backgroundColor: Theme.of(context).extension<CustomColors>()!.background2,
+                selectedItemColor: Theme.of(context).colorScheme.primary,
+                unselectedItemColor: Theme.of(context).colorScheme.outline,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: badges.Badge(
+                        position: badges.BadgePosition.topEnd(top: 0, end: -1),
+                        showBadge: badgeState.unreadReceivedClaims > 0 || badgeState.unreadNews > 0,
+                        child: const Icon(Icons.home),
+                      ),
+                      label: AppLocalizations.of(context)!.homeBottomBarTab),
+                  BottomNavigationBarItem(
+                      icon: const Icon(Icons.search), label: AppLocalizations.of(context)!.searchBottomBarTab),
+                  BottomNavigationBarItem(
+                      icon: const Icon(Icons.add_circle_outline_outlined),
+                      label: AppLocalizations.of(context)!.insertBottomBarTab),
+                  BottomNavigationBarItem(
+                      icon: badges.Badge(
+                        showBadge: badgeState.hasUnreadChats,
+                        position: badges.BadgePosition.topEnd(top: 0, end: -1),
+                        child: const Icon(Icons.mail),
+                      ),
+                      label: AppLocalizations.of(context)!.inboxBottomBarTab),
+                  BottomNavigationBarItem(
+                      icon: const Icon(Icons.person), label: AppLocalizations.of(context)!.profileBottomBarTab),
+                ],
               ),
             );
           },
